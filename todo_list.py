e8e92319f7332d78c480cb22b098d50b3768f20c
samukelo <samukelo@pop-os.lan>
2026-05-06 02:25:47 +0200

    something broke, we need to fix it

--- todo_list.py
+++ todo_list.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 def load_tasks():
     try:
@@ -9,30 +10,36 @@ def load_tasks():
 
 def save_tasks(tasks):
     with open("tasks.json", "w") as file:
-        json.dump(tasks, file)
+        json.dump(tasks, file, indent=4)
         
 def add_task_logic(tasks, task):
-    tasks.append(task)
+    now_time = datetime.now().strftime("%H:%M:%S")
+    now_date = datetime.now().strftime("%Y-%m-%d")
+    tasks.append(f"{now_date}\n{now_time}\n{task}")
 
 def remove_task_logic(tasks, index):
     if 0 <= index < len(tasks):
         return tasks.pop(index)
     return None     
         
-
 def show_tasks(tasks):
     if not tasks:
         print("No tasks yet.")
     else:
         print("Your Tasks:")
         for i, task in enumerate(tasks, 1):
-            print(f"{i}. {task}")
+            print(f"{i}. {task['date']}")
+            print(f"   {task['time']}")
+            print(f"   {task['task']}\n")
 
-def add_task(tasks):
-    task = input("Enter a new task: ")
-    add_task_logic(tasks, task)
-    save_tasks(tasks)
-    print("Task added!")
+def add_task_logic(tasks, task):
+    now = datetime.now()
+    
+    tasks.append({
+        "date": now.strftime("%Y-%m-%d"),
+        "time": now.strftime("%H:%M:%S"),
+        "task": task
+    })
 
 def remove_task(tasks):
     show_tasks(tasks)
@@ -52,7 +59,12 @@ def main():
     tasks = load_tasks()
 
     while True:
+        now = datetime.now()
+        current_time = now.strftime("\n%d-%m-%y \n%H:%M:%S")
+        
         print("\n      TO-DO LIST     \n")
+        
+        print(f"Date & Time: {current_time}\n")
         print("1. View Tasks")
         print("2. Add Task")
         print("3. Remove Task")
@@ -62,10 +74,12 @@ def main():
 
         if choice == "1":
             show_tasks(tasks)
+            input("\nPress Enter to continue...")
         elif choice == "2":
             add_task(tasks)
         elif choice == "3":
             remove_task(tasks)
+            input("\nPress Enter to continue...")
         elif choice == "4":
             print("\nGoodbye, see you next time!\n")
             break
